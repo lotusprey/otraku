458f3e8d1e35a4ba07ba030195a557bc609cf8ef
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2020-09-08 00:25:59 +0300

    New headers.

---
+++ lib/tools/headers/collection_control_header.dart
@@ -0,0 +1,120 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:otraku/providers/anime_collection.dart';
+import 'package:otraku/providers/collection_provider.dart';
+import 'package:otraku/providers/manga_collection.dart';
+import 'package:otraku/providers/theming.dart';
+import 'package:otraku/tools/headers/header_refresh_button.dart';
+import 'package:otraku/tools/headers/header_search_button.dart';
+import 'package:otraku/tools/navigation/title_segmented_control.dart';
+import 'package:otraku/tools/overlays/collection_sort_sheet.dart';
+import 'package:provider/provider.dart';
+
+class CollectionControlHeader extends StatelessWidget {
+  final bool isAnime;
+
+  const CollectionControlHeader(this.isAnime);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: false,
+      floating: true,
+      delegate: _CollectionControlHeaderDelegate(context, isAnime),
+    );
+  }
+}
+
+class _CollectionControlHeaderDelegate
+    implements SliverPersistentHeaderDelegate {
+  static const _height = 100.0;
+
+  CollectionProvider _collection;
+  Palette _palette;
+
+  _CollectionControlHeaderDelegate(BuildContext context, bool isAnime) {
+    if (isAnime) {
+      _collection = Provider.of<AnimeCollection>(context);
+    } else {
+      _collection = Provider.of<MangaCollection>(context);
+    }
+    _palette = Provider.of<Theming>(context, listen: false).palette;
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    Map<String, Object> segmentedControlPairs = {'All': -1};
+    final allNames = _collection.names;
+    for (int i = 0; i < allNames.length; i++) {
+      segmentedControlPairs[allNames[i]] = i;
+    }
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          height: _height,
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+          color: _palette.background.withAlpha(210),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TitleSegmentedControl(
+                function: (value) => _collection.listIndex = value,
+                pairs: segmentedControlPairs,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  HeaderSearchButton(_collection, _palette),
+                  IconButton(
+                    icon: const Icon(LineAwesomeIcons.sort),
+                    color: _palette.faded,
+                    iconSize: Palette.ICON_MEDIUM,
+                    onPressed: () => showModalBottomSheet(
+                      context: context,
+                      builder: (ctx) =>
+                          CollectionSortSheet(_collection.isAnimeCollection),
+                      backgroundColor: Colors.transparent,
+                      isScrollControlled: true,
+                    ),
+                  ),
+                  HeaderRefreshButton(
+                    listenable: _collection,
+                    readable: _collection.isAnimeCollection
+                        ? Provider.of<AnimeCollection>(context, listen: false)
+                        : Provider.of<MangaCollection>(context, listen: false),
+                    palette: _palette,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => _height;
+
+  @override
+  double get minExtent => _height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+
+  @override
+  FloatingHeaderSnapConfiguration get snapConfiguration => null;
+
+  @override
+  OverScrollHeaderStretchConfiguration get stretchConfiguration => null;
+}
---
+++ lib/tools/headers/explore_control_header.dart
@@ -0,0 +1,147 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:otraku/pages/pushable/filter_page.dart';
+import 'package:otraku/providers/explorable_media.dart';
+import 'package:otraku/providers/theming.dart';
+import 'package:otraku/tools/headers/header_refresh_button.dart';
+import 'package:otraku/tools/headers/header_search_button.dart';
+import 'package:otraku/tools/navigation/title_segmented_control.dart';
+import 'package:otraku/tools/overlays/explore_sort_sheet.dart';
+import 'package:provider/provider.dart';
+
+class ExploreControlHeader extends StatelessWidget {
+  const ExploreControlHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: false,
+      floating: true,
+      delegate: _ExploreControlHeaderDelegate(context),
+    );
+  }
+}
+
+class _ExploreControlHeaderDelegate implements SliverPersistentHeaderDelegate {
+  static const _height = 100.0;
+
+  Palette _palette;
+
+  _ExploreControlHeaderDelegate(BuildContext context) {
+    _palette = Provider.of<Theming>(context, listen: false).palette;
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          height: _height,
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+          color: _palette.background.withAlpha(210),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TitleSegmentedControl(
+                function: (value) => Provider.of<ExplorableMedia>(
+                  context,
+                  listen: false,
+                ).type = value,
+                pairs: const {'Anime': 'ANIME', 'Manga': 'MANGA'},
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  HeaderSearchButton(
+                    Provider.of<ExplorableMedia>(context),
+                    _palette,
+                  ),
+                  _FilterButton(_palette),
+                  IconButton(
+                    icon: const Icon(LineAwesomeIcons.sort),
+                    color: _palette.faded,
+                    iconSize: Palette.ICON_MEDIUM,
+                    onPressed: () => showModalBottomSheet(
+                      context: context,
+                      builder: (ctx) => ExploreSortSheet(),
+                      backgroundColor: Colors.transparent,
+                      isScrollControlled: true,
+                    ),
+                  ),
+                  HeaderRefreshButton(
+                    listenable: Provider.of<ExplorableMedia>(context),
+                    readable:
+                        Provider.of<ExplorableMedia>(context, listen: false),
+                    palette: _palette,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => _height;
+
+  @override
+  double get minExtent => _height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+
+  @override
+  FloatingHeaderSnapConfiguration get snapConfiguration => null;
+
+  @override
+  OverScrollHeaderStretchConfiguration get stretchConfiguration => null;
+}
+
+class _FilterButton extends StatelessWidget {
+  final Palette palette;
+
+  _FilterButton(this.palette);
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider.of<ExplorableMedia>(context).areFiltersActive()
+        ? GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute(builder: (_) => FilterPage()),
+            ),
+            onLongPress: Provider.of<ExplorableMedia>(context, listen: false)
+                .clearGenreTagFilters,
+            child: Container(
+              width: 48,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: palette.accent,
+              ),
+              child: const Icon(
+                LineAwesomeIcons.filter,
+                size: Palette.ICON_MEDIUM,
+                color: Colors.white,
+              ),
+            ),
+          )
+        : IconButton(
+            icon: const Icon(LineAwesomeIcons.filter),
+            color: palette.faded,
+            iconSize: Palette.ICON_MEDIUM,
+            onPressed: () => Navigator.of(context).push(
+              CupertinoPageRoute(builder: (_) => FilterPage()),
+            ),
+          );
+  }
+}

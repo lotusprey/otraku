11ccf74bb6b5474d803d1a253d7def7766d45f69
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2020-11-13 15:21:46 +0200

    Minor clean up.

--- lib/tools/headers/header_search_bar.dart
+++ lib/tools/headers/header_search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
--- lib/tools/headers/media_page_header.dart
+++ lib/tools/headers/media_page_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:otraku/enums/media_list_status_enum.dart';

73927ce04dad348d69f07cec9fa12e2e4eacf688
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2020-11-19 14:45:19 +0200

    Finished abstracting the LargeTileGrid.

--- lib/pages/tabs/explore_tab.dart
+++ lib/pages/tabs/explore_tab.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:otraku/controllers/explorable.dart';
+import 'package:otraku/enums/browsable_enum.dart';
 import 'package:otraku/tools/blossom_loader.dart';
 import 'package:otraku/tools/headers/explore_header.dart';
-import 'package:otraku/tools/layouts/explore_grid.dart';
+import 'package:otraku/tools/layouts/result_grid.dart';
 import 'package:otraku/tools/headers/headline_header.dart';
 
 class ExploreTab extends StatefulWidget {
@@ -26,11 +27,35 @@ class _ExploreTabState extends State<ExploreTab> {
       slivers: [
         const HeadlineHeader('Explore', false),
         ExploreHeader(widget.scrollCtrl),
-        ExploreGrid(),
+        _ExploreGrid(),
         _ConditionalLoader(),
       ],
     );
   }
+}
+
+class _ExploreGrid extends StatelessWidget {
+  void _loadMore() {
+    final explorable = Get.find<Explorable>();
+    if (explorable.hasNextPage && !explorable.isLoading) {
+      explorable.loadMore();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Obx(() {
+        final results = Get.find<Explorable>().results;
+
+        if (results.length == 0) {
+          return NoResults();
+        }
+
+        if (results[0].browsable == Browsable.studios) {
+          return TitleList(results, _loadMore);
+        }
+
+        return LargeGrid(results, _loadMore);
+      });
 }
 
 class _ConditionalLoader extends StatelessWidget {

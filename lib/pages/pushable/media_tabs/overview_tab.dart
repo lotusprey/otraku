107d2889d56dfe9efb0535db0e3120f1dac3fb86
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2020-11-30 14:22:37 +0200

    Fixes.

--- lib/pages/pushable/media_tabs/overview_tab.dart
+++ lib/pages/pushable/media_tabs/overview_tab.dart
@@ -8,204 +8,208 @@ import 'package:otraku/tools/overlays/dialogs.dart';
 import 'package:otraku/tools/page_transition.dart';
 
 class OverviewTab extends StatelessWidget {
+  static const _space = SizedBox(height: 10);
+
   final MediaOverview overview;
 
   OverviewTab(this.overview);
 
   @override
-  Widget build(BuildContext context) => SliverList(
-        delegate: SliverChildListDelegate(
-          [
-            if (overview.description != null)
-              InputFieldStructure(
-                enforceHeight: false,
-                title: 'Description',
-                body: GestureDetector(
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: Config.BORDER_RADIUS,
-                    ),
-                    child: Text(
-                      overview.description,
-                      style: Theme.of(context).textTheme.bodyText1,
-                      overflow: TextOverflow.fade,
-                      maxLines: 5,
+  Widget build(BuildContext context) {
+    final tileCount = (MediaQuery.of(context).size.width - 10) ~/ 150;
+    final tileAspectRatio =
+        (((MediaQuery.of(context).size.width - 10) / tileCount) - 10) / 51.0;
+    final infoTitles = [
+      'Format',
+      'Status',
+      'Episodes',
+      'Duration',
+      'Chapters',
+      'Volumes',
+      'Start Date',
+      'End Date',
+      'Season',
+      'Average Score',
+      'Mean Score',
+      'Popularity',
+      'Favourites',
+      'Source',
+      'Origin'
+    ];
+    final infoChildren = [
+      overview.format,
+      overview.status,
+      overview.episodes,
+      overview.duration,
+      overview.chapters,
+      overview.volumes,
+      overview.startDate,
+      overview.endDate,
+      overview.season,
+      overview.averageScore,
+      overview.meanScore,
+      overview.popularity,
+      overview.favourites,
+      overview.source,
+      overview.countryOfOrigin,
+    ];
+
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          if (overview.description != null)
+            InputFieldStructure(
+              enforceHeight: false,
+              title: 'Description',
+              body: GestureDetector(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: Config.BORDER_RADIUS,
+                  ),
+                  child: Text(
+                    overview.description,
+                    style: Theme.of(context).textTheme.bodyText1,
+                    overflow: TextOverflow.fade,
+                    maxLines: 5,
+                  ),
+                ),
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (_) => PopUpAnimation(
+                    TextDialog(
+                      title: 'Description',
+                      text: overview.description,
                     ),
                   ),
-                  onTap: () => showDialog(
-                    context: context,
-                    builder: (_) => PopUpAnimation(
-                      TextDialog(
-                        title: 'Description',
-                        text: overview.description,
+                ),
+              ),
+            ),
+          Text('Info', style: Theme.of(context).textTheme.subtitle1),
+          const SizedBox(height: 5),
+          GridView.count(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            semanticChildCount: infoTitles.length,
+            crossAxisCount: tileCount,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: tileAspectRatio,
+            children: [
+              for (int i = 0; i < infoChildren.length; i++)
+                if (infoChildren[i] != null)
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: Config.BORDER_RADIUS,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    child: InputFieldStructure(
+                      enforceHeight: false,
+                      enforcePadding: false,
+                      title: infoTitles[i],
+                      body: Text(
+                        infoChildren[i].toString(),
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
                     ),
                   ),
-                ),
+            ],
+          ),
+          if (overview.studios != null &&
+              overview.studios.item1.isNotEmpty) ...[
+            _space,
+            _ScrollTile(
+              title: 'Studios',
+              builder: (index) => _StudioLink(
+                overview.studios.item1[index],
+                overview.studios.item2[index],
               ),
-            _WrapInfo(
-              [
-                'Format',
-                'Status',
-                'Episodes',
-                'Duration',
-                'Chapters',
-                'Volumes',
-                'Start Date',
-                'End Date',
-                'Season',
-                'Average Score',
-                'Mean Score',
-                'Popularity',
-                'Favourites',
-                'Source',
-                'Origin',
-              ],
-              [
-                overview.format,
-                overview.status,
-                overview.episodes,
-                overview.duration,
-                overview.chapters,
-                overview.volumes,
-                overview.startDate,
-                overview.endDate,
-                overview.season,
-                overview.averageScore,
-                overview.meanScore,
-                overview.popularity,
-                overview.favourites,
-                overview.source,
-                overview.countryOfOrigin,
-              ],
+              itemCount: overview.studios.item1.length,
             ),
-            if (overview.studios != null &&
-                overview.studios.item1.length > 0) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'Studios',
-                builder: (index) => _StudioLink(
-                  overview.studios.item1[index],
-                  overview.studios.item2[index],
-                ),
-                itemCount: overview.studios.item1.length,
-              ),
-            ],
-            if (overview.producers != null &&
-                overview.producers.item1.length > 0) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'Producers',
-                builder: (index) => _StudioLink(
-                  overview.producers.item1[index],
-                  overview.producers.item2[index],
-                ),
-                itemCount: overview.producers.item1.length,
-              ),
-            ],
-            if (overview.romajiTitle != null) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'Romaji',
-                builder: (_) => GestureDetector(
-                  child: Text(
-                    overview.romajiTitle,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  onLongPress: () => Clipboard.setData(
-                    ClipboardData(text: overview.romajiTitle),
-                  ),
-                ),
-                itemCount: 1,
-              ),
-            ],
-            if (overview.englishTitle != null) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'English',
-                builder: (_) => GestureDetector(
-                  child: Text(
-                    overview.englishTitle,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  onLongPress: () => Clipboard.setData(
-                    ClipboardData(text: overview.englishTitle),
-                  ),
-                ),
-                itemCount: 1,
-              ),
-            ],
-            if (overview.nativeTitle != null) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'Native',
-                builder: (_) => GestureDetector(
-                  child: Text(
-                    overview.nativeTitle,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  onLongPress: () => Clipboard.setData(
-                    ClipboardData(text: overview.nativeTitle),
-                  ),
-                ),
-                itemCount: 1,
-              ),
-            ],
-            if (overview.synonyms != null && overview.synonyms.length > 0) ...[
-              const SizedBox(height: 10),
-              _ScrollTile(
-                title: 'Synonyms',
-                builder: (index) => GestureDetector(
-                  child: Text(
-                    overview.synonyms[index],
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  onLongPress: () => Clipboard.setData(
-                    ClipboardData(text: overview.synonyms[index]),
-                  ),
-                ),
-                itemCount: overview.synonyms.length,
-              ),
-            ],
           ],
-        ),
-      );
-}
-
-class _WrapInfo extends StatelessWidget {
-  final List<String> titles;
-  final List<dynamic> children;
-
-  _WrapInfo(this.titles, this.children);
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 10,
-      runSpacing: 10,
-      children: [
-        for (int i = 0; i < children.length; i++)
-          if (children[i] != null)
-            Container(
-              width: 120,
-              padding: Config.PADDING,
-              decoration: BoxDecoration(
-                borderRadius: Config.BORDER_RADIUS,
-                color: Theme.of(context).primaryColor,
+          if (overview.producers != null &&
+              overview.producers.item1.isNotEmpty) ...[
+            _space,
+            _ScrollTile(
+              title: 'Producers',
+              builder: (index) => _StudioLink(
+                overview.producers.item1[index],
+                overview.producers.item2[index],
               ),
-              child: InputFieldStructure(
-                enforceHeight: false,
-                enforcePadding: false,
-                title: titles[i],
-                body: Text(
-                  children[i].toString(),
+              itemCount: overview.producers.item1.length,
+            ),
+          ],
+          if (overview.romajiTitle != null) ...[
+            _space,
+            _ScrollTile(
+              title: 'Romaji',
+              builder: (_) => GestureDetector(
+                child: Text(
+                  overview.romajiTitle,
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
+                onLongPress: () => Clipboard.setData(
+                  ClipboardData(text: overview.romajiTitle),
+                ),
               ),
+              itemCount: 1,
             ),
-      ],
+          ],
+          if (overview.englishTitle != null) ...[
+            _space,
+            _ScrollTile(
+              title: 'English',
+              builder: (_) => GestureDetector(
+                child: Text(
+                  overview.englishTitle,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                onLongPress: () => Clipboard.setData(
+                  ClipboardData(text: overview.englishTitle),
+                ),
+              ),
+              itemCount: 1,
+            ),
+          ],
+          if (overview.nativeTitle != null) ...[
+            _space,
+            _ScrollTile(
+              title: 'Native',
+              builder: (_) => GestureDetector(
+                child: Text(
+                  overview.nativeTitle,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                onLongPress: () => Clipboard.setData(
+                  ClipboardData(text: overview.nativeTitle),
+                ),
+              ),
+              itemCount: 1,
+            ),
+          ],
+          if (overview.synonyms != null && overview.synonyms.isNotEmpty) ...[
+            _space,
+            _ScrollTile(
+              title: 'Synonyms',
+              builder: (index) => GestureDetector(
+                child: Text(
+                  overview.synonyms[index],
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                onLongPress: () => Clipboard.setData(
+                  ClipboardData(text: overview.synonyms[index]),
+                ),
+              ),
+              itemCount: overview.synonyms.length,
+            ),
+          ],
+        ],
+      ),
     );
   }
 }

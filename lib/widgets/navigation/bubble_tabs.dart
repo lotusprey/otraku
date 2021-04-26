d40db5c4bf4d53c9d83bbcf9eebed6f162a19ba9
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-04-26 13:47:06 +0300

    Code simplification.

--- lib/widgets/navigation/bubble_tabs.dart
+++ lib/widgets/navigation/bubble_tabs.dart
@@ -7,7 +7,6 @@ class BubbleTabs<T> extends StatefulWidget {
   final T initial;
   final Function(T) onNewValue;
   final Function(T) onSameValue;
-  final bool shrinkWrap;
 
   const BubbleTabs({
     required this.options,
@@ -15,7 +14,6 @@ class BubbleTabs<T> extends StatefulWidget {
     required this.initial,
     required this.onNewValue,
     required this.onSameValue,
-    this.shrinkWrap = true,
   });
 
   @override
@@ -28,40 +26,41 @@ class _BubbleTabsState<T> extends State<BubbleTabs<T>> {
   @override
   Widget build(BuildContext context) => SizedBox(
         height: 35,
-        child: ListView.builder(
-          shrinkWrap: widget.shrinkWrap,
-          scrollDirection: Axis.horizontal,
-          physics: Config.PHYSICS,
-          itemBuilder: (_, index) => GestureDetector(
-            onTap: () {
-              if (index != _index) {
-                setState(() => _index = index);
-                widget.onNewValue(widget.values[index]);
-              } else
-                widget.onSameValue(widget.values[index]);
-            },
-            child: AnimatedContainer(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              duration: const Duration(milliseconds: 300),
-              decoration: BoxDecoration(
-                color: index != _index
-                    ? Colors.transparent
-                    : Theme.of(context).accentColor,
-                borderRadius: Config.BORDER_RADIUS,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < widget.options.length; i++) ...[
+              GestureDetector(
+                onTap: () {
+                  if (_index != i) {
+                    setState(() => _index = i);
+                    widget.onNewValue(widget.values[i]);
+                  } else
+                    widget.onSameValue(widget.values[i]);
+                },
+                child: AnimatedContainer(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  duration: const Duration(milliseconds: 300),
+                  decoration: BoxDecoration(
+                    color: _index != i
+                        ? Colors.transparent
+                        : Theme.of(context).accentColor,
+                    borderRadius: Config.BORDER_RADIUS,
+                  ),
+                  child: Text(
+                    widget.options[i],
+                    style: _index != i
+                        ? Theme.of(context).textTheme.headline5
+                        : Theme.of(context)
+                            .textTheme
+                            .headline5!
+                            .copyWith(color: Theme.of(context).backgroundColor),
+                  ),
+                ),
               ),
-              child: Text(
-                widget.options[index],
-                style: index != _index
-                    ? Theme.of(context).textTheme.headline5
-                    : Theme.of(context)
-                        .textTheme
-                        .headline5!
-                        .copyWith(color: Theme.of(context).backgroundColor),
-              ),
-            ),
-          ),
-          itemCount: widget.options.length,
+            ]
+          ],
         ),
       );
 

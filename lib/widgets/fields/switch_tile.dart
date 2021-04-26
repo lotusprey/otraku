5c87e750e01dfa87f1787777ebe4c738791b5174
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-04-26 14:20:27 +0300

    Fixed padding.

--- lib/widgets/fields/switch_tile.dart
+++ lib/widgets/fields/switch_tile.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class SwitchTile extends StatefulWidget {
   final String title;
-  final bool? initialValue;
+  final bool initialValue;
   final Function(bool) onChanged;
 
   SwitchTile({
     required this.title,
-    required this.initialValue,
     required this.onChanged,
+    required this.initialValue,
   });
 
   @override
@@ -16,16 +16,16 @@ class SwitchTile extends StatefulWidget {
 }
 
 class _SwitchTileState extends State<SwitchTile> {
-  bool? _value;
+  late bool _value;
 
   @override
   Widget build(BuildContext context) => SwitchListTile.adaptive(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+        contentPadding: const EdgeInsets.all(0),
         title: Text(
           widget.title,
           style: Theme.of(context).textTheme.bodyText1,
         ),
-        value: _value!,
+        value: _value,
         onChanged: (val) {
           setState(() => _value = val);
           widget.onChanged(val);

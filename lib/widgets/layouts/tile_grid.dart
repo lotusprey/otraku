81b03b194d190cb2d63026c6a182a9859f7aa9ac
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-04-27 17:31:00 +0300

    Sliver and non-sliver versions. Keys.

--- lib/widgets/layouts/tile_grid.dart
+++ lib/widgets/layouts/tile_grid.dart
@@ -9,62 +9,47 @@ import 'package:otraku/widgets/layouts/sliver_grid_delegates.dart';
 class TileGrid extends StatelessWidget {
   final List<BrowseResultModel> tileData;
   final TileModel tileModel;
+  final bool sliver;
+  final ScrollController? scrollCtrl;
 
   TileGrid({
     required this.tileData,
     required this.tileModel,
-  });
+    this.sliver = false,
+    this.scrollCtrl,
+    UniqueKey? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final sidePadding = MediaQuery.of(context).size.width > 620
         ? (MediaQuery.of(context).size.width - 600) / 2.0
         : 10.0;
+    final padding = EdgeInsets.only(
+      left: sidePadding,
+      right: sidePadding,
+      top: 15,
+    );
+
+    if (!sliver)
+      return GridView.builder(
+        padding: padding,
+        controller: scrollCtrl,
+        physics: Config.PHYSICS,
+        itemCount: tileData.length,
+        itemBuilder: (_, i) => _Tile(tileData[i], tileModel),
+        gridDelegate: SliverGridDelegateWithMaxWidthAndAddedHeight(
+          maxWidth: tileModel.maxWidth,
+          additionalHeight: tileModel.textHeight,
+          rawWHRatio: tileModel.imgWHRatio,
+        ),
+      );
 
     return SliverPadding(
-      padding: EdgeInsets.only(
-        left: sidePadding,
-        right: sidePadding,
-        top: 15,
-      ),
+      padding: padding,
       sliver: SliverGrid(
         delegate: SliverChildBuilderDelegate(
-          (_, index) => BrowseIndexer(
-            browsable: tileData[index].browsable,
-            id: tileData[index].id,
-            imageUrl: tileData[index].imageUrl,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Hero(
-                    tag: tileData[index].id,
-                    child: ClipRRect(
-                      borderRadius: Config.BORDER_RADIUS,
-                      child: Container(
-                        color: tileModel.needsBackground
-                            ? Theme.of(context).primaryColor
-                            : null,
-                        child: FadeImage(
-                          tileData[index].imageUrl,
-                          fit: tileModel.fit,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                SizedBox(
-                  height: tileModel.textHeight,
-                  child: Text(
-                    tileData[index].text1,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          (_, i) => _Tile(tileData[i], tileModel),
           childCount: tileData.length,
         ),
         gridDelegate: SliverGridDelegateWithMaxWidthAndAddedHeight(
@@ -72,6 +57,52 @@ class TileGrid extends StatelessWidget {
           additionalHeight: tileModel.textHeight,
           rawWHRatio: tileModel.imgWHRatio,
         ),
+      ),
+    );
+  }
+}
+
+class _Tile extends StatelessWidget {
+  final BrowseResultModel data;
+  final TileModel tileModel;
+  _Tile(this.data, this.tileModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return BrowseIndexer(
+      browsable: data.browsable,
+      id: data.id,
+      imageUrl: data.imageUrl,
+      child: Column(
+        children: [
+          Expanded(
+            child: Hero(
+              tag: data.id,
+              child: ClipRRect(
+                borderRadius: Config.BORDER_RADIUS,
+                child: Container(
+                  color: tileModel.needsBackground
+                      ? Theme.of(context).primaryColor
+                      : null,
+                  child: FadeImage(
+                    data.imageUrl,
+                    fit: tileModel.fit,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: tileModel.textHeight,
+            child: Text(
+              data.text1,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/layouts/title_list.dart
+++ lib/widgets/layouts/title_list.dart
@@ -1,33 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:otraku/models/helper_models/browse_result_model.dart';
+import 'package:otraku/utils/config.dart';
 import 'package:otraku/widgets/browse_indexer.dart';
 
 class TitleList extends StatelessWidget {
   final List<BrowseResultModel> results;
+  final ScrollController? scrollCtrl;
+  final bool sliver;
 
-  TitleList(this.results);
+  TitleList(this.results, {this.sliver = true, this.scrollCtrl, UniqueKey? key})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SliverPadding(
+  Widget build(BuildContext context) {
+    if (sliver)
+      return SliverPadding(
         padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
         sliver: SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
-            (_, index) => BrowseIndexer(
-              browsable: results[index].browsable,
-              id: results[index].id,
-              imageUrl: results[index].text1,
-              child: Hero(
-                tag: results[index].id,
-                child: Text(
-                  results[index].text1,
-                  style: Theme.of(context).textTheme.headline2,
-                  maxLines: 2,
-                ),
-              ),
-            ),
+            (_, i) => _Tile(results[i]),
             childCount: results.length,
           ),
           itemExtent: 60,
         ),
       );
+
+    return ListView.builder(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
+      controller: scrollCtrl,
+      physics: Config.PHYSICS,
+      itemExtent: 60,
+      itemCount: results.length,
+      itemBuilder: (_, i) => _Tile(results[i]),
+    );
+  }
+}
+
+class _Tile extends StatelessWidget {
+  final BrowseResultModel data;
+  _Tile(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return BrowseIndexer(
+      browsable: data.browsable,
+      id: data.id,
+      imageUrl: data.text1,
+      child: Hero(
+        tag: data.id,
+        child: Text(
+          data.text1,
+          style: Theme.of(context).textTheme.headline2,
+          maxLines: 2,
+        ),
+      ),
+    );
+  }
 }

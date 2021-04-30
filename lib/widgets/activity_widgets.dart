a1976dd23fb21cfa4aa0ed0975eaa33b8184466d
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-04-30 13:10:04 +0300

    Simplification.

--- lib/widgets/activity_widgets.dart
+++ lib/widgets/activity_widgets.dart
@@ -84,17 +84,12 @@ class UserActivity extends StatelessWidget {
   }
 }
 
-class ActivityBox extends StatefulWidget {
+class ActivityBox extends StatelessWidget {
   final ActivityModel activity;
-  final bool canPush;
+  final bool canPushPage;
 
-  ActivityBox(this.activity, {this.canPush = true});
+  ActivityBox(this.activity, {this.canPushPage = true});
 
-  @override
-  _ActivityBoxState createState() => _ActivityBoxState();
-}
-
-class _ActivityBoxState extends State<ActivityBox> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -106,120 +101,24 @@ class _ActivityBoxState extends State<ActivityBox> {
       ),
       child: Column(
         children: [
-          if (widget.activity.type == ActivityType.ANIME_LIST ||
-              widget.activity.type == ActivityType.MANGA_LIST)
-            BrowseIndexer(
-              id: widget.activity.mediaId!,
-              imageUrl: widget.activity.mediaImage,
-              browsable: widget.activity.mediaType!,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: 108),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: Config.BORDER_RADIUS,
-                      child: FadeImage(
-                        widget.activity.mediaImage,
-                        width: 70,
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: Config.PADDING,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Flexible(
-                              child: RichText(
-                                overflow: TextOverflow.fade,
-                                text: TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: widget.activity.text,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText1,
-                                    ),
-                                    TextSpan(
-                                      text: widget.activity.mediaTitle,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            if (widget.activity.mediaFormat != null)
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  widget.activity.mediaFormat!,
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
+          if (activity.type == ActivityType.ANIME_LIST ||
+              activity.type == ActivityType.MANGA_LIST)
+            _MediaBox(activity)
           else
             UnconstrainedBox(
               constrainedAxis: Axis.horizontal,
               alignment: Alignment.topLeft,
-              child: HtmlContent(widget.activity.text),
+              child: HtmlContent(activity.text),
             ),
           const SizedBox(height: 5),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                widget.activity.createdAt,
+                activity.createdAt,
                 style: Theme.of(context).textTheme.subtitle2,
               ),
-              Row(
-                children: [
-                  _SubscribeIcon(widget.activity),
-                  const SizedBox(width: 10),
-                  Tooltip(
-                    message: 'Replies',
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {
-                        if (widget.canPush)
-                          Get.toNamed(
-                            ActivityPage.ROUTE,
-                            arguments: [
-                              widget.activity.id,
-                              widget.activity,
-                              (ActivityModel other) => setState(
-                                    () => widget.activity.updateFrom(other),
-                                  ),
-                            ],
-                            parameters: {'id': widget.activity.id.toString()},
-                          );
-                      },
-                      child: Row(
-                        children: [
-                          Text(
-                            widget.activity.replyCount.toString(),
-                            style: Theme.of(context).textTheme.subtitle2,
-                          ),
-                          const SizedBox(width: 5),
-                          const Icon(
-                            Ionicons.chatbox,
-                            size: Style.ICON_SMALL,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  _ActivityLikeIcon(widget.activity),
-                ],
-              ),
+              _InteractionButtons(activity, canPushPage),
             ],
           ),
         ],
@@ -228,71 +127,159 @@ class _ActivityBoxState extends State<ActivityBox> {
   }
 }
 
-class _ActivityLikeIcon extends StatefulWidget {
+class _InteractionButtons extends StatefulWidget {
   final ActivityModel activity;
-  _ActivityLikeIcon(this.activity);
+  final bool canPushPage;
+  _InteractionButtons(this.activity, this.canPushPage);
 
   @override
-  _ActivityLikeIconState createState() => _ActivityLikeIconState();
+  __InteractionButtonsState createState() => __InteractionButtonsState();
 }
 
-class _ActivityLikeIconState extends State<_ActivityLikeIcon> {
+class __InteractionButtonsState extends State<_InteractionButtons> {
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: !widget.activity.isLiked ? 'Like' : 'Unlike',
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => Activity.toggleActivityLike(widget.activity)
-            .then((_) => setState(() {})),
-        child: Row(
-          children: [
-            Text(
-              widget.activity.likeCount.toString(),
-              style: !widget.activity.isLiked
-                  ? Theme.of(context).textTheme.subtitle2
-                  : Theme.of(context)
-                      .textTheme
-                      .subtitle2!
-                      .copyWith(color: Theme.of(context).errorColor),
-            ),
-            const SizedBox(width: 5),
-            Icon(
-              Icons.favorite,
+    return Row(
+      children: [
+        Tooltip(
+          message: !widget.activity.isSubscribed ? 'Subscribe' : 'Unsubscribe',
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () => Activity.toggleSubscription(widget.activity)
+                .then((_) => setState(() {})),
+            child: Icon(
+              Ionicons.notifications,
               size: Style.ICON_SMALL,
-              color:
-                  widget.activity.isLiked ? Theme.of(context).errorColor : null,
+              color: !widget.activity.isSubscribed
+                  ? null
+                  : Theme.of(context).accentColor,
             ),
-          ],
+          ),
         ),
-      ),
+        const SizedBox(width: 10),
+        Tooltip(
+          message: 'Replies',
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              if (widget.canPushPage)
+                Get.toNamed(
+                  ActivityPage.ROUTE,
+                  arguments: [
+                    widget.activity.id,
+                    widget.activity,
+                    (ActivityModel other) => setState(
+                          () => widget.activity.updateFrom(other),
+                        ),
+                  ],
+                  parameters: {'id': widget.activity.id.toString()},
+                );
+            },
+            child: Row(
+              children: [
+                Text(
+                  widget.activity.replyCount.toString(),
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+                const SizedBox(width: 5),
+                const Icon(
+                  Ionicons.chatbox,
+                  size: Style.ICON_SMALL,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Tooltip(
+          message: !widget.activity.isLiked ? 'Like' : 'Unlike',
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () => Activity.toggleActivityLike(widget.activity)
+                .then((_) => setState(() {})),
+            child: Row(
+              children: [
+                Text(
+                  widget.activity.likeCount.toString(),
+                  style: !widget.activity.isLiked
+                      ? Theme.of(context).textTheme.subtitle2
+                      : Theme.of(context)
+                          .textTheme
+                          .subtitle2!
+                          .copyWith(color: Theme.of(context).errorColor),
+                ),
+                const SizedBox(width: 5),
+                Icon(
+                  Icons.favorite,
+                  size: Style.ICON_SMALL,
+                  color: widget.activity.isLiked
+                      ? Theme.of(context).errorColor
+                      : null,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
-class _SubscribeIcon extends StatefulWidget {
+class _MediaBox extends StatelessWidget {
   final ActivityModel activity;
-  _SubscribeIcon(this.activity);
+  _MediaBox(this.activity);
 
-  @override
-  _SubscribeIconState createState() => _SubscribeIconState();
-}
-
-class _SubscribeIconState extends State<_SubscribeIcon> {
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: !widget.activity.isSubscribed ? 'Subscribe' : 'Unsubscribe',
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => Activity.toggleSubscription(widget.activity)
-            .then((_) => setState(() {})),
-        child: Icon(
-          Ionicons.notifications,
-          size: Style.ICON_SMALL,
-          color: !widget.activity.isSubscribed
-              ? null
-              : Theme.of(context).accentColor,
+    return BrowseIndexer(
+      id: activity.mediaId!,
+      imageUrl: activity.mediaImage,
+      browsable: activity.mediaType!,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: 108),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: Config.BORDER_RADIUS,
+              child: FadeImage(activity.mediaImage, width: 70),
+            ),
+            Expanded(
+              child: Padding(
+                padding: Config.PADDING,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      child: RichText(
+                        overflow: TextOverflow.fade,
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: activity.text,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                            TextSpan(
+                              text: activity.mediaTitle,
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    if (activity.mediaFormat != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          activity.mediaFormat!,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

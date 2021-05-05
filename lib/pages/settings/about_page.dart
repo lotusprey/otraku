66b9434671038079e8c37d8801c8223bd379605f
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-05-05 14:57:36 +0300

    Updated app version.

--- lib/pages/settings/about_page.dart
+++ lib/pages/settings/about_page.dart
@@ -28,7 +28,7 @@ class AboutTab extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 5),
           child: Text(
-            'Otraku - v. 1.0.6',
+            'Otraku - v. 1.0.7',
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.headline5,
           ),

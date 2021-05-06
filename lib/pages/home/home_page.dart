8995beb398422e3484adf61ed79747f12779f161
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-05-06 12:44:00 +0300

    Confirmation dialog before going back from the home page.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -8,6 +8,7 @@ import 'package:otraku/pages/home/user_page.dart';
 import 'package:otraku/utils/config.dart';
 import 'package:otraku/widgets/navigation/custom_drawer.dart';
 import 'package:otraku/widgets/navigation/nav_bar.dart';
+import 'package:otraku/widgets/overlays/dialogs.dart';
 
 import '../../utils/client.dart';
 
@@ -48,31 +49,65 @@ class HomePage extends StatelessWidget {
       const SizedBox(),
     ];
 
-    return ValueListenableBuilder<int>(
-      valueListenable: Config.index,
-      builder: (_, index, __) => Scaffold(
-        extendBody: true,
-        drawerScrimColor: Theme.of(context).primaryColor.withAlpha(150),
-        bottomNavigationBar: NavBar(
-          options: {
-            'Feed': Ionicons.file_tray_outline,
-            'Anime': Ionicons.film_outline,
-            'Manga': Ionicons.bookmark_outline,
-            'Explore': Ionicons.compass_outline,
-            'Profile': Ionicons.person_outline,
-          },
-          onChanged: (page) => Config.setIndex(page),
-          initial: index,
-        ),
-        drawer: drawers[index],
-        body: SafeArea(
-          bottom: false,
-          child: AnimatedSwitcher(
-            duration: Config.TAB_SWITCH_DURATION,
-            child: tabs[index],
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: ValueListenableBuilder<int>(
+        valueListenable: Config.index,
+        builder: (_, index, __) => Scaffold(
+          extendBody: true,
+          drawerScrimColor: Theme.of(context).primaryColor.withAlpha(150),
+          bottomNavigationBar: NavBar(
+            options: {
+              'Feed': Ionicons.file_tray_outline,
+              'Anime': Ionicons.film_outline,
+              'Manga': Ionicons.bookmark_outline,
+              'Explore': Ionicons.compass_outline,
+              'Profile': Ionicons.person_outline,
+            },
+            onChanged: (page) => Config.setIndex(page),
+            initial: index,
+          ),
+          drawer: drawers[index],
+          body: SafeArea(
+            bottom: false,
+            child: AnimatedSwitcher(
+              duration: Config.TAB_SWITCH_DURATION,
+              child: tabs[index],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<bool> _onWillPop(BuildContext ctx) async {
+    bool ok = false;
+
+    await showPopUp(
+      ctx,
+      AlertDialog(
+        shape: const RoundedRectangleBorder(borderRadius: Config.BORDER_RADIUS),
+        backgroundColor: Theme.of(ctx).primaryColor,
+        title: Text('Exit?'),
+        actions: [
+          TextButton(
+            child: Text(
+              'Never',
+              style: TextStyle(color: Theme.of(ctx).dividerColor),
+            ),
+            onPressed: () => Navigator.of(ctx).pop(),
+          ),
+          TextButton(
+            child: Text('Yes'),
+            onPressed: () {
+              ok = true;
+              Navigator.of(ctx).pop();
+            },
+          ),
+        ],
+      ),
+    );
+
+    return ok;
   }
 }

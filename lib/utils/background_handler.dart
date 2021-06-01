1903afb060bf1de03f51016db03936373c9bc8ab
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-06-01 19:04:58 +0300

    Minor change - added const.

--- lib/utils/background_handler.dart
+++ lib/utils/background_handler.dart
@@ -27,7 +27,7 @@ class BackgroundHandler {
     _didInit = true;
 
     _notificationPlugin.initialize(
-      InitializationSettings(
+      const InitializationSettings(
         android: AndroidInitializationSettings('notification_icon'),
         iOS: IOSInitializationSettings(),
         macOS: MacOSInitializationSettings(),
@@ -190,18 +190,16 @@ void _show(NotificationModel model, String title, String payload) =>
       model.id,
       title,
       model.texts.join(),
-      _details,
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'NOTIFICATIONS',
+          'Notifications',
+          'All Notifications',
+          color: Color(0xFF45A0F2),
+        ),
+      ),
       payload: payload,
     );
-
-const _details = NotificationDetails(
-  android: AndroidNotificationDetails(
-    'NOTIFICATIONS',
-    'Notifications',
-    'All Notifications',
-    color: Color(0xFF45A0F2),
-  ),
-);
 
 const _countQuery = 'query Count {Viewer {unreadNotificationCount}}';
 

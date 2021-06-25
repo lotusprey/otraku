186ec330bc47fa2e4a6ec69c4045e741a06ef452
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-06-25 19:27:49 +0300

    Removed the triangle clips from activities to make them smaller.

--- lib/widgets/activity_widgets.dart
+++ lib/widgets/activity_widgets.dart
@@ -75,14 +75,6 @@ class UserActivity extends StatelessWidget {
           ],
         ),
         const SizedBox(height: 5),
-        ClipPath(
-          clipper: TriangleClip(),
-          child: Container(
-            width: 50,
-            height: 10,
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
         Container(
           margin: const EdgeInsets.only(bottom: 10),
           padding: Config.PADDING,
@@ -272,11 +264,11 @@ class MediaBox extends StatelessWidget {
                           children: [
                             TextSpan(
                               text: activity.text,
-                              style: Theme.of(context).textTheme.bodyText2,
+                              style: Theme.of(context).textTheme.subtitle1,
                             ),
                             TextSpan(
                               text: activity.mediaTitle,
-                              style: Theme.of(context).textTheme.bodyText1,
+                              style: Theme.of(context).textTheme.bodyText2,
                             ),
                           ],
                         ),
@@ -285,7 +277,10 @@ class MediaBox extends StatelessWidget {
                     if (activity.mediaFormat != null)
                       Padding(
                         padding: const EdgeInsets.only(top: 5),
-                        child: Text(activity.mediaFormat!),
+                        child: Text(
+                          activity.mediaFormat!,
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
                       ),
                   ],
                 ),

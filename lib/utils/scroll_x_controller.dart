cb384a85aa23c9abb441e10e56c66aaafb45232e
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-07-13 17:08:02 +0300

    Fixed an assertion with multiple attached scrollviews to a controller.

--- lib/utils/scroll_x_controller.dart
+++ lib/utils/scroll_x_controller.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 abstract class ScrollxController extends GetxController {
-  final scrollCtrl = ScrollController();
+  final scrollCtrl = _ScrollController();
 
   Future<void> scrollTo(double offset) async {
     if (!scrollCtrl.hasClients) return;
-    if (scrollCtrl.offset > offset + 100) scrollCtrl.jumpTo(offset + 100);
-    await scrollCtrl.animateTo(
+
+    if (scrollCtrl.last.pixels > offset + 100)
+      scrollCtrl.last.jumpTo(offset + 100);
+
+    await scrollCtrl.last.animateTo(
       offset,
       duration: const Duration(milliseconds: 200),
       curve: Curves.decelerate,
@@ -21,7 +24,7 @@ abstract class ScrollxController extends GetxController {
   bool _canLoad = true;
 
   Future<void> _listener() async {
-    if (scrollCtrl.offset > scrollCtrl.position.maxScrollExtent - 100 &&
+    if (scrollCtrl.last.pixels > scrollCtrl.last.maxScrollExtent - 100 &&
         _canLoad &&
         hasNextPage) {
       _canLoad = false;
@@ -42,5 +45,20 @@ abstract class ScrollxController extends GetxController {
     scrollCtrl.removeListener(_listener);
     scrollCtrl.dispose();
     super.onClose();
+  }
+}
+
+class _ScrollController extends ScrollController {
+  // Returns the last attached ScrollPosition.
+  // This was necessary, because it's possible that there would be multiple
+  // pages using the same Get controller and consecutively, using the same
+  // ScrollController. The position getter works with only 1 ScrollPosition
+  // attached, so that required the addition of lastPosition.
+  ScrollPosition get last {
+    assert(
+      positions.isNotEmpty,
+      'ScrollController not attached to any scroll views.',
+    );
+    return positions.last;
   }
 }

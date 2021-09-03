09cced13ebc1d9e5b721bfa649bcc16978e5c0eb
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-09-03 19:39:07 +0300

    Turned studio list into studio grid.

--- lib/views/favourites_view.dart
+++ lib/views/favourites_view.dart
@@ -4,7 +4,7 @@ import 'package:otraku/controllers/favourites_controller.dart';
 import 'package:otraku/enums/explorable.dart';
 import 'package:otraku/models/user_model.dart';
 import 'package:otraku/widgets/layouts/tile_grid.dart';
-import 'package:otraku/widgets/layouts/title_list.dart';
+import 'package:otraku/widgets/layouts/title_grid.dart';
 import 'package:otraku/widgets/nav_scaffold.dart';
 import 'package:otraku/widgets/navigation/app_bars.dart';
 import 'package:otraku/widgets/navigation/nav_bar.dart';
@@ -31,7 +31,7 @@ class FavouritesView extends StatelessWidget {
           appBar: ShadowAppBar(title: 'Favourite ${favourites.pageName}'),
           child: favourites.favourites.isNotEmpty
               ? favourites.pageIndex == UserModel.STUDIO_FAV
-                  ? TitleList(
+                  ? TitleGrid(
                       favourites.favourites,
                       scrollCtrl: favourites.scrollCtrl,
                       key: favourites.key,
--- lib/widgets/layouts/title_grid.dart
+++ lib/widgets/layouts/title_grid.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:otraku/models/explorable_model.dart';
 import 'package:otraku/utils/config.dart';
 import 'package:otraku/widgets/explore_indexer.dart';
+import 'package:otraku/widgets/layouts/sliver_grid_delegates.dart';
 import 'package:otraku/widgets/navigation/nav_bar.dart';
 
-class TitleList extends StatelessWidget {
+class TitleGrid extends StatelessWidget {
   final List<ExplorableModel> results;
   final ScrollController? scrollCtrl;
 
-  TitleList(this.results, {this.scrollCtrl, UniqueKey? key}) : super(key: key);
+  TitleGrid(this.results, {this.scrollCtrl, UniqueKey? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,25 +22,29 @@ class TitleList extends StatelessWidget {
       bottom: scrollCtrl == null ? 0 : NavBar.offset(context),
       top: 15,
     );
+    const gridDelegate = SliverGridDelegateWithMinWidthAndFixedHeight(
+      minWidth: 230,
+      height: 50,
+    );
 
     if (scrollCtrl != null)
-      return ListView.builder(
+      return GridView.builder(
         padding: padding,
         controller: scrollCtrl,
         physics: Config.PHYSICS,
-        itemExtent: 60,
         itemCount: results.length,
+        gridDelegate: gridDelegate,
         itemBuilder: (_, i) => _Tile(results[i]),
       );
 
     return SliverPadding(
       padding: padding,
-      sliver: SliverFixedExtentList(
+      sliver: SliverGrid(
+        gridDelegate: gridDelegate,
         delegate: SliverChildBuilderDelegate(
           (_, i) => _Tile(results[i]),
           childCount: results.length,
         ),
-        itemExtent: 60,
       ),
     );
   }
@@ -59,7 +64,8 @@ class _Tile extends StatelessWidget {
         tag: data.id,
         child: Text(
           data.text1,
-          style: Theme.of(context).textTheme.headline2,
+          style: Theme.of(context).textTheme.headline3,
+          overflow: TextOverflow.fade,
           maxLines: 2,
         ),
       ),

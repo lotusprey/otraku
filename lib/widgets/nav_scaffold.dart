879119518c51a0212dc8af265dba44db76ab9b3e
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-10-12 14:28:26 +0300

    Testing new tab transition.

--- lib/widgets/nav_scaffold.dart
+++ lib/widgets/nav_scaffold.dart
@@ -4,7 +4,7 @@ import 'package:otraku/widgets/drag_detector.dart';
 import 'package:otraku/widgets/navigation/app_bars.dart';
 import 'package:otraku/widgets/navigation/nav_bar.dart';
 
-class NavScaffold extends StatelessWidget {
+class NavScaffold extends StatefulWidget {
   final Widget child;
   final Map<String, IconData> items;
   final void Function(int) setPage;
@@ -12,11 +12,11 @@ class NavScaffold extends StatelessWidget {
   final Widget? floating;
   final ShadowAppBar? appBar;
 
-  // When a tab swipe is detected, NavScaffold will try to switch the subtab of
-  // the page, instead of the tab itself. For 'go right' true is passed to the
-  // callback and for 'go left' - false. The returned value determines whether
-  // the subtab has been switched. If not (the end of the subtab carousel has
-  // been reached), NavScaffold will switch the tab.
+  /// When a tab swipe is detected, [NavScaffold] will try to switch the subtab
+  /// of the page, instead of the tab itself. For 'go right' true is passed to
+  /// the callback and for 'go left' - false. The returned value determines
+  /// whether the subtab has been switched. If not (the end of the subtab
+  /// carousel has been reached), [NavScaffold] will switch the tab.
   final bool Function(bool)? trySubtab;
 
   const NavScaffold({
@@ -30,36 +30,70 @@ class NavScaffold extends StatelessWidget {
   });
 
   @override
+  State<NavScaffold> createState() => _NavScaffoldState();
+}
+
+class _NavScaffoldState extends State<NavScaffold> {
+  late Tween<Offset> _slide;
+
+  @override
+  void initState() {
+    super.initState();
+    _slide = Tween(begin: Offset.zero, end: Offset.zero);
+  }
+
+  @override
+  void didUpdateWidget(covariant NavScaffold oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.index > oldWidget.index)
+      _slide = Tween(begin: const Offset(0.5, 0), end: Offset.zero);
+    else if (widget.index < oldWidget.index)
+      _slide = Tween(begin: const Offset(-0.5, 0), end: Offset.zero);
+    else
+      _slide = Tween(begin: Offset.zero, end: Offset.zero);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final body = DragDetector(
       child: AnimatedSwitcher(
+        child: widget.child,
+        switchInCurve: Curves.easeOutExpo,
+        switchOutCurve: Curves.easeInExpo,
         duration: const Duration(milliseconds: 200),
-        child: child,
+        transitionBuilder: (child, animation) => FadeTransition(
+          opacity: animation,
+          child: SlideTransition(
+            position: _slide.animate(animation),
+            child: child,
+          ),
+        ),
       ),
       onSwipe: (goRight) {
         // Try to switch the subtab, instead of the tab.
-        if (trySubtab?.call(goRight) ?? false) return;
+        if (widget.trySubtab?.call(goRight) ?? false) return;
 
         if (goRight) {
-          if (index < items.length - 1) setPage(index + 1);
+          if (widget.index < widget.items.length - 1)
+            widget.setPage(widget.index + 1);
         } else {
-          if (index > 0) setPage(index - 1);
+          if (widget.index > 0) widget.setPage(widget.index - 1);
         }
       },
     );
 
     return Scaffold(
-      appBar: appBar,
-      floatingActionButton: floating,
+      extendBody: true,
+      appBar: widget.appBar,
+      floatingActionButton: widget.floating,
       floatingActionButtonLocation:
           (Config.storage.read(Config.LEFT_HANDED) ?? false)
               ? FloatingActionButtonLocation.startFloat
               : FloatingActionButtonLocation.endFloat,
-      extendBody: true,
       bottomNavigationBar: NavBar(
-        items: items,
-        initial: index,
-        onChanged: setPage,
+        items: widget.items,
+        initial: widget.index,
+        onChanged: widget.setPage,
       ),
       body: SafeArea(bottom: false, child: body),
     );

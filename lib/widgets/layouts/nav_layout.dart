811770d3e3d608b99269686989438724ed65d2f8
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2021-10-27 17:15:06 +0300

    Removed custom animation from NavLayout, because a different one will be used in the future.

--- lib/widgets/layouts/nav_layout.dart
+++ lib/widgets/layouts/nav_layout.dart
@@ -3,9 +3,19 @@ import 'package:otraku/utils/config.dart';
 import 'package:otraku/widgets/drag_detector.dart';
 import 'package:otraku/widgets/navigation/app_bars.dart';
 
-/// A [Scaffold] with a custom [Scaffold.bottomNavigationBar] and a tab
-/// transition animation.
-class NavLayout extends StatefulWidget {
+/// A [Scaffold] with a custom [Scaffold.bottomNavigationBar]
+/// and a tab transition animation.
+class NavLayout extends StatelessWidget {
+  const NavLayout({
+    required this.child,
+    required this.items,
+    required this.onChanged,
+    this.index = 0,
+    this.trySubtab,
+    this.floating,
+    this.appBar,
+  });
+
   final Widget child;
   final Map<String, IconData> items;
   final void Function(int) onChanged;
@@ -20,80 +30,36 @@ class NavLayout extends StatefulWidget {
   /// carousel has been reached), [NavLayout] will switch the tab.
   final bool Function(bool)? trySubtab;
 
-  const NavLayout({
-    required this.child,
-    required this.items,
-    required this.onChanged,
-    this.trySubtab,
-    this.index = 0,
-    this.floating,
-    this.appBar,
-  });
-
-  @override
-  State<NavLayout> createState() => _NavLayoutState();
-
   // At the bottom of a page there should be this offset
-  // in order to avoid obstruction by the navbar.
+  // in order to avoid obstruction by the bottom bar.
   static double offset(BuildContext ctx) =>
       MediaQuery.of(ctx).viewPadding.bottom + 60;
-}
-
-class _NavLayoutState extends State<NavLayout> {
-  late Tween<Offset> _slide;
-
-  @override
-  void initState() {
-    super.initState();
-    _slide = Tween(begin: Offset.zero, end: Offset.zero);
-  }
-
-  @override
-  void didUpdateWidget(covariant NavLayout oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.index > oldWidget.index)
-      _slide = Tween(begin: const Offset(0.5, 0), end: Offset.zero);
-    else if (widget.index < oldWidget.index)
-      _slide = Tween(begin: const Offset(-0.5, 0), end: Offset.zero);
-    else
-      _slide = Tween(begin: Offset.zero, end: Offset.zero);
-  }
 
   @override
   Widget build(BuildContext context) {
     final body = DragDetector(
       child: AnimatedSwitcher(
-        child: widget.child,
-        switchInCurve: Curves.easeOutExpo,
-        switchOutCurve: Curves.easeInExpo,
         duration: const Duration(milliseconds: 200),
-        transitionBuilder: (child, animation) => FadeTransition(
-          opacity: animation,
-          child: SlideTransition(
-            position: _slide.animate(animation),
-            child: child,
-          ),
-        ),
+        child: child,
       ),
       onSwipe: (goRight) {
         // Try to switch the subtab, instead of the tab.
-        if (widget.trySubtab?.call(goRight) ?? false) return;
+        if (trySubtab?.call(goRight) ?? false) return;
 
         if (goRight) {
-          if (widget.index < widget.items.length - 1)
-            widget.onChanged(widget.index + 1);
+          if (index < items.length - 1) onChanged(index + 1);
         } else {
-          if (widget.index > 0) widget.onChanged(widget.index - 1);
+          if (index > 0) onChanged(index - 1);
         }
       },
     );
 
-    final full = MediaQuery.of(context).size.width > widget.items.length * 130;
+    final full = MediaQuery.of(context).size.width > items.length * 130;
 
     return Scaffold(
       extendBody: true,
-      appBar: widget.appBar,
-      floatingActionButton: widget.floating,
+      appBar: appBar,
+      floatingActionButton: floating,
       floatingActionButtonLocation:
           (Config.storage.read(Config.LEFT_HANDED) ?? false)
               ? FloatingActionButtonLocation.startFloat
@@ -111,11 +77,11 @@ class _NavLayoutState extends State<NavLayout> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                for (int i = 0; i < widget.items.length; i++)
+                for (int i = 0; i < items.length; i++)
                   GestureDetector(
                     behavior: HitTestBehavior.opaque,
                     onTap: () {
-                      if (i != widget.index) widget.onChanged(i);
+                      if (i != index) onChanged(i);
                     },
                     child: SizedBox(
                       height: double.infinity,
@@ -124,16 +90,16 @@ class _NavLayoutState extends State<NavLayout> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
-                            widget.items.values.elementAt(i),
-                            color: i != widget.index
+                            items.values.elementAt(i),
+                            color: i != index
                                 ? null
                                 : Theme.of(context).colorScheme.secondary,
                           ),
                           if (full) ...[
                             const SizedBox(width: 5),
                             Text(
-                              widget.items.keys.elementAt(i),
-                              style: i != widget.index
+                              items.keys.elementAt(i),
+                              style: i != index
                                   ? Theme.of(context).textTheme.subtitle1
                                   : Theme.of(context).textTheme.bodyText1,
                             ),

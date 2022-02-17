4d699bd5370c9a1c4737c82bd12610db9007c971
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2022-02-17 17:56:05 +0200

    Updated versioning.

--- lib/views/settings_about_view.dart
+++ lib/views/settings_about_view.dart
@@ -32,7 +32,7 @@ class SettingsAboutView extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 5),
           child: Text(
-            'Otraku - v. 1.1.6',
+            'Otraku - v. 1.1.7',
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.headline2,
           ),

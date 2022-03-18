87681c8852b15f7898bf69733f233168ffb14588
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2022-03-18 11:38:37 +0200

    Changed connection grid design.

--- lib/widgets/layouts/connections_grid.dart
+++ lib/widgets/layouts/connections_grid.dart
@@ -17,18 +17,18 @@ class ConnectionsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) => SliverGrid(
         delegate: SliverChildBuilderDelegate(
-          (_, i) => _MediaConnectionTile(connections[i], preferredSubtitle),
+          (_, i) => _ConnectionTile(connections[i], preferredSubtitle),
           childCount: connections.length,
         ),
         gridDelegate: const SliverGridDelegateWithMinWidthAndFixedHeight(
           minWidth: 300,
-          height: 110,
+          height: 115,
         ),
       );
 }
 
-class _MediaConnectionTile extends StatelessWidget {
-  _MediaConnectionTile(this.item, this.preferredSubtitle);
+class _ConnectionTile extends StatelessWidget {
+  _ConnectionTile(this.item, this.preferredSubtitle);
 
   final ConnectionModel item;
   final String? preferredSubtitle;
@@ -43,109 +43,90 @@ class _MediaConnectionTile extends StatelessWidget {
           break;
         }
 
-    return Align(
-      alignment: Alignment.topCenter,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          borderRadius: Consts.BORDER_RAD_MIN,
-          color: Theme.of(context).colorScheme.surface,
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: ExploreIndexer(
-                id: item.id,
-                explorable: item.type,
-                imageUrl: item.imageUrl,
-                child: Container(
-                  color: Colors.transparent,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ClipRRect(
-                        child: FadeImage(item.imageUrl, width: 75),
-                        borderRadius:
-                            BorderRadius.horizontal(left: Consts.RADIUS_MIN),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: Consts.PADDING,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  item.title,
-                                  overflow: TextOverflow.fade,
-                                ),
-                              ),
-                              if (item.subtitle != null)
-                                Text(
-                                  item.subtitle!,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.fade,
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: Consts.BORDER_RAD_MIN,
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      child: Row(
+        children: [
+          ExploreIndexer(
+            id: item.id,
+            explorable: item.type,
+            imageUrl: item.imageUrl,
+            child: ClipRRect(
+              child: FadeImage(item.imageUrl, width: 80),
+              borderRadius: Consts.BORDER_RAD_MIN,
             ),
-            if (item.other.length > index)
-              Expanded(
-                child: ExploreIndexer(
-                  id: item.other[index].id,
-                  explorable: item.other[index].type,
-                  imageUrl: item.other[index].imageUrl,
-                  child: Container(
-                    color: Colors.transparent,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+          ),
+          Expanded(
+            child: Padding(
+              padding: Consts.PADDING,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: Padding(
-                            padding: Consts.PADDING,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Flexible(
-                                  child: Text(
-                                    item.other[index].title,
-                                    overflow: TextOverflow.fade,
-                                    textAlign: TextAlign.end,
-                                  ),
-                                ),
-                                if (item.other[index].subtitle != null)
-                                  Text(
-                                    item.other[index].subtitle!,
-                                    style:
-                                        Theme.of(context).textTheme.subtitle2,
-                                  ),
-                              ],
-                            ),
+                        Flexible(
+                          child: Text(
+                            item.title,
+                            maxLines: 2,
+                            overflow: TextOverflow.fade,
                           ),
                         ),
-                        ClipRRect(
-                          child: FadeImage(
-                            item.other[index].imageUrl,
-                            width: 75,
+                        if (item.subtitle != null)
+                          Text(
+                            item.subtitle!,
+                            maxLines: 2,
+                            overflow: TextOverflow.fade,
+                            style: Theme.of(context).textTheme.subtitle2,
                           ),
-                          borderRadius:
-                              BorderRadius.horizontal(right: Consts.RADIUS_MIN),
-                        ),
                       ],
                     ),
                   ),
-                ),
+                  if (item.other.length > index) ...[
+                    const SizedBox(height: 3),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            item.other[index].title,
+                            maxLines: 2,
+                            textAlign: TextAlign.end,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                        if (item.other[index].subtitle != null)
+                          Text(
+                            item.other[index].subtitle!,
+                            maxLines: 2,
+                            overflow: TextOverflow.fade,
+                            style: Theme.of(context).textTheme.subtitle2,
+                          ),
+                      ],
+                    ),
+                  ],
+                ],
               ),
-          ],
-        ),
+            ),
+          ),
+          if (item.other.length > index)
+            ExploreIndexer(
+              id: item.other[index].id,
+              explorable: item.other[index].type,
+              imageUrl: item.other[index].imageUrl,
+              child: ClipRRect(
+                child: FadeImage(item.other[index].imageUrl, width: 80),
+                borderRadius: Consts.BORDER_RAD_MIN,
+              ),
+            ),
+        ],
       ),
     );
   }

c96b545ed9f7a64ab73bdd69a6ce17f3ae08dbe5
Hris-Zhav <35681808+lotusgate@users.noreply.github.com>
2022-07-28 14:26:39 +0300

    Minor visual adjustments in the theme preview.

--- lib/settings/settings_app_tab.dart
+++ lib/settings/settings_app_tab.dart
@@ -20,119 +20,123 @@ class SettingsAppTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: CustomScrollView(
-          controller: scrollCtrl,
-          slivers: [
-            SliverToBoxAdapter(
-              child: SizedBox(height: PageLayout.of(context).topOffset + 10),
+    return CustomScrollView(
+      controller: scrollCtrl,
+      slivers: [
+        SliverToBoxAdapter(
+          child: SizedBox(height: PageLayout.of(context).topOffset + 10),
+        ),
+        const ThemePreview(isDark: false),
+        const ThemePreview(isDark: true),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithMinWidthAndFixedHeight(
+              minWidth: 160,
+              height: 75,
             ),
-            ThemePreview(isDark: false),
-            ThemePreview(isDark: true),
-            SliverGrid(
-              gridDelegate: const SliverGridDelegateWithMinWidthAndFixedHeight(
-                minWidth: 160,
-                height: 75,
+            delegate: SliverChildListDelegate.fixed([
+              DropDownField<ThemeMode>(
+                title: 'Theme Mode',
+                value: Settings().themeMode,
+                items: const {
+                  'Auto': ThemeMode.system,
+                  'Light': ThemeMode.light,
+                  'Dark': ThemeMode.dark,
+                },
+                onChanged: (val) => Settings().themeMode = val,
               ),
-              delegate: SliverChildListDelegate.fixed([
-                DropDownField<ThemeMode>(
-                  title: 'Theme Mode',
-                  value: Settings().themeMode,
-                  items: const {
-                    'Auto': ThemeMode.system,
-                    'Light': ThemeMode.light,
-                    'Dark': ThemeMode.dark,
-                  },
-                  onChanged: (val) => Settings().themeMode = val,
-                ),
-                DropDownField<int>(
-                  title: 'Startup Page',
-                  value: Settings().defaultHomeTab,
-                  items: const {
-                    'Inbox': HomeView.INBOX,
-                    'Anime List': HomeView.ANIME_LIST,
-                    'Manga List': HomeView.MANGA_LIST,
-                    'Explore': HomeView.EXPLORE,
-                    'Profile': HomeView.USER,
-                  },
-                  onChanged: (val) => Settings().defaultHomeTab = val,
-                ),
-                DropDownField<EntrySort>(
-                  title: 'Default Anime Sort',
-                  value: Settings().defaultAnimeSort,
-                  items: Map.fromIterable(
-                    EntrySort.values,
-                    key: (v) => Convert.clarifyEnum((v as EntrySort).name)!,
-                  ),
-                  onChanged: (val) => Settings().defaultAnimeSort = val,
-                ),
-                DropDownField<EntrySort>(
-                  title: 'Default Manga Sort',
-                  value: Settings().defaultMangaSort,
-                  items: Map.fromIterable(
-                    EntrySort.values,
-                    key: (v) => Convert.clarifyEnum((v as EntrySort).name)!,
-                  ),
-                  onChanged: (val) => Settings().defaultMangaSort = val,
-                ),
-                DropDownField<MediaSort>(
-                  title: 'Default Explore Sort',
-                  value: Settings().defaultExploreSort,
-                  items: Map.fromIterable(
-                    MediaSort.values,
-                    key: (v) => Convert.clarifyEnum((v as MediaSort).name)!,
-                  ),
-                  onChanged: (val) => Settings().defaultExploreSort = val,
-                ),
-                DropDownField<Explorable>(
-                  title: 'Default Explorable',
-                  value: Settings().defaultExplorable,
-                  items: Map.fromIterable(
-                    Explorable.values,
-                    key: (v) => Convert.clarifyEnum((v as Explorable).name)!,
-                  ),
-                  onChanged: (val) => Settings().defaultExplorable = val,
-                ),
-                DropDownField<String>(
-                  title: 'Image Quality',
-                  value: Settings().imageQuality,
-                  items: const {
-                    'Very High': 'extraLarge',
-                    'High': 'large',
-                    'Medium': 'medium',
-                  },
-                  onChanged: (val) => Settings().imageQuality = val,
-                ),
-              ]),
-            ),
-            SliverGrid(
-              gridDelegate: const SliverGridDelegateWithMinWidthAndFixedHeight(
-                minWidth: 200,
-                mainAxisSpacing: 0,
-                crossAxisSpacing: 20,
-                height: Consts.tapTargetSize,
+              DropDownField<int>(
+                title: 'Startup Page',
+                value: Settings().defaultHomeTab,
+                items: const {
+                  'Inbox': HomeView.INBOX,
+                  'Anime List': HomeView.ANIME_LIST,
+                  'Manga List': HomeView.MANGA_LIST,
+                  'Explore': HomeView.EXPLORE,
+                  'Profile': HomeView.USER,
+                },
+                onChanged: (val) => Settings().defaultHomeTab = val,
               ),
-              delegate: SliverChildListDelegate.fixed([
-                CheckBoxField(
-                  title: 'Left-Handed Mode',
-                  initial: Settings().leftHanded,
-                  onChanged: (val) => Settings().leftHanded = val,
+              DropDownField<EntrySort>(
+                title: 'Default Anime Sort',
+                value: Settings().defaultAnimeSort,
+                items: Map.fromIterable(
+                  EntrySort.values,
+                  key: (v) => Convert.clarifyEnum((v as EntrySort).name)!,
                 ),
-                CheckBoxField(
-                  title: '12 Hour Clock',
-                  initial: Settings().analogueClock,
-                  onChanged: (val) => Settings().analogueClock = val,
+                onChanged: (val) => Settings().defaultAnimeSort = val,
+              ),
+              DropDownField<EntrySort>(
+                title: 'Default Manga Sort',
+                value: Settings().defaultMangaSort,
+                items: Map.fromIterable(
+                  EntrySort.values,
+                  key: (v) => Convert.clarifyEnum((v as EntrySort).name)!,
                 ),
-                CheckBoxField(
-                  title: 'Confirm Exit',
-                  initial: Settings().confirmExit,
-                  onChanged: (val) => Settings().confirmExit = val,
+                onChanged: (val) => Settings().defaultMangaSort = val,
+              ),
+              DropDownField<MediaSort>(
+                title: 'Default Explore Sort',
+                value: Settings().defaultExploreSort,
+                items: Map.fromIterable(
+                  MediaSort.values,
+                  key: (v) => Convert.clarifyEnum((v as MediaSort).name)!,
                 ),
-              ]),
+                onChanged: (val) => Settings().defaultExploreSort = val,
+              ),
+              DropDownField<Explorable>(
+                title: 'Default Explorable',
+                value: Settings().defaultExplorable,
+                items: Map.fromIterable(
+                  Explorable.values,
+                  key: (v) => Convert.clarifyEnum((v as Explorable).name)!,
+                ),
+                onChanged: (val) => Settings().defaultExplorable = val,
+              ),
+              DropDownField<String>(
+                title: 'Image Quality',
+                value: Settings().imageQuality,
+                items: const {
+                  'Very High': 'extraLarge',
+                  'High': 'large',
+                  'Medium': 'medium',
+                },
+                onChanged: (val) => Settings().imageQuality = val,
+              ),
+            ]),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithMinWidthAndFixedHeight(
+              minWidth: 200,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 20,
+              height: Consts.tapTargetSize,
             ),
-            const SliverFooter(),
-          ],
-        ));
+            delegate: SliverChildListDelegate.fixed([
+              CheckBoxField(
+                title: 'Left-Handed Mode',
+                initial: Settings().leftHanded,
+                onChanged: (val) => Settings().leftHanded = val,
+              ),
+              CheckBoxField(
+                title: '12 Hour Clock',
+                initial: Settings().analogueClock,
+                onChanged: (val) => Settings().analogueClock = val,
+              ),
+              CheckBoxField(
+                title: 'Confirm Exit',
+                initial: Settings().confirmExit,
+                onChanged: (val) => Settings().confirmExit = val,
+              ),
+            ]),
+          ),
+        ),
+        const SliverFooter(),
+      ],
+    );
   }
 }
--- lib/widgets/containers/theme_preview.dart
+++ lib/widgets/containers/theme_preview.dart
@@ -13,10 +13,9 @@ class ThemePreview extends StatefulWidget {
 }
 
 class _ThemePreviewState extends State<ThemePreview> {
-  late int activeNow =
-      widget.isDark ? Settings().darkTheme : Settings().lightTheme;
+  late int index = widget.isDark ? Settings().darkTheme : Settings().lightTheme;
 
-  late Iterable<MapEntry<String, ColorScheme>> themes = widget.isDark
+  late final themes = widget.isDark
       ? Theming.schemes.entries
           .where((entry) => entry.value.brightness == Brightness.dark)
       : Theming.schemes.entries
@@ -24,19 +23,18 @@ class _ThemePreviewState extends State<ThemePreview> {
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, int> colorSchemeMap = <String, int>{};
+    final colorSchemeMap = <String, int>{};
     for (int i = 0; i < Theming.schemes.length; i++)
       colorSchemeMap[Theming.schemes.keys.elementAt(i)] = i;
 
     return SliverToBoxAdapter(
       child: Padding(
-        padding: EdgeInsets.only(bottom: 10),
+        padding: const EdgeInsets.only(bottom: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 5, left: 5),
+              padding: const EdgeInsets.only(left: 10, bottom: 5),
               child: Text(
                 widget.isDark ? "Dark Theme" : "Light Theme",
                 style: Theme.of(context).textTheme.subtitle1,
@@ -46,15 +44,15 @@ class _ThemePreviewState extends State<ThemePreview> {
               height: 200,
               child: ListView(
                 scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(horizontal: 5),
                 children: [
-                  for (var item in themes)
+                  for (final item in themes)
                     _ThemeCard(
-                      key: Key(item.key),
                       name: item.key,
                       scheme: item.value,
-                      active: activeNow == colorSchemeMap[item.key],
+                      active: index == colorSchemeMap[item.key],
                       onTap: (name) {
-                        setState(() => activeNow = colorSchemeMap[name]!);
+                        setState(() => index = colorSchemeMap[name]!);
                         widget.isDark
                             ? Settings().darkTheme = colorSchemeMap[name]!
                             : Settings().lightTheme = colorSchemeMap[name]!;
@@ -72,7 +70,6 @@ class _ThemePreviewState extends State<ThemePreview> {
 
 class _ThemeCard extends StatelessWidget {
   const _ThemeCard({
-    super.key,
     required this.name,
     required this.scheme,
     required this.active,
@@ -82,149 +79,140 @@ class _ThemeCard extends StatelessWidget {
   final String name;
   final bool active;
   final ColorScheme scheme;
-  final void Function(String name) onTap;
+  final void Function(String) onTap;
 
   @override
   Widget build(BuildContext context) {
-    final double borderWidth = active ? 2 : 1;
-    final Color borderColor = active ? scheme.primary : scheme.surfaceVariant;
+    final borderWidth = active ? 3.0 : 1.0;
+    final borderColor = active ? scheme.primary : scheme.surfaceVariant;
 
     return GestureDetector(
       onTap: () => onTap(name),
       child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: Column(
-            children: [
-              Container(
-                width: 120,
-                height: 180,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  color: scheme.background,
-                  border: Border.all(color: borderColor, width: borderWidth),
-                  borderRadius: Consts.borderRadiusMin,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 10,
-                            width: 60,
-                            decoration: BoxDecoration(
-                              color: scheme.onBackground,
-                              borderRadius: Consts.borderRadiusMax,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: scheme.surface,
-                              borderRadius: Consts.borderRadiusMin,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    height: 8,
-                                    width: 40,
-                                    decoration: BoxDecoration(
-                                      color: scheme.surfaceVariant,
-                                      borderRadius: Consts.borderRadiusMax,
-                                    ),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Container(
-                                    height: 6,
-                                    width: 110,
-                                    decoration: BoxDecoration(
-                                      color: scheme.onSurface,
-                                      borderRadius: Consts.borderRadiusMax,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          children: [
+            Container(
+              width: 120,
+              height: 180,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: scheme.background,
+                border: Border.all(color: borderColor, width: borderWidth),
+                borderRadius: Consts.borderRadiusMin,
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 10,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: scheme.onBackground,
+                          borderRadius: Consts.borderRadiusMax,
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 7.5, bottom: 5),
-                          child: Container(
-                            width: 16,
-                            height: 16,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: scheme.primary,
-                            ),
-                            child: Center(
-                              child: Container(
-                                width: 6,
-                                height: 2,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: Consts.borderRadiusMin,
-                                  color: scheme.onPrimary,
-                                ),
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 40,
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: scheme.surface,
+                          borderRadius: Consts.borderRadiusMin,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 8,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: scheme.surfaceVariant,
+                                borderRadius: Consts.borderRadiusMax,
                               ),
                             ),
+                            const SizedBox(height: 5),
+                            Container(
+                              height: 6,
+                              width: 110,
+                              decoration: BoxDecoration(
+                                color: scheme.onSurface,
+                                borderRadius: Consts.borderRadiusMax,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 16,
+                        height: 16,
+                        margin: const EdgeInsets.only(right: 7, bottom: 7),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: scheme.primary,
+                        ),
+                        child: Center(
+                          child: Container(
+                            width: 6,
+                            height: 2,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              borderRadius: Consts.borderRadiusMin,
+                              color: scheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          height: 8,
+                          width: 8,
+                          decoration: BoxDecoration(
+                            color: scheme.primary,
+                            shape: BoxShape.rectangle,
+                          ),
+                        ),
+                        Container(
+                          height: 8,
+                          width: 8,
+                          decoration: BoxDecoration(
+                            color: scheme.surfaceVariant,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        Container(
+                          height: 8,
+                          width: 8,
+                          decoration: BoxDecoration(
+                            color: scheme.surfaceVariant,
+                            shape: BoxShape.circle,
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            height: 8,
-                            width: 8,
-                            decoration: BoxDecoration(
-                              color: scheme.primary,
-                              shape: BoxShape.rectangle,
-                            ),
-                          ),
-                          Container(
-                            height: 8,
-                            width: 8,
-                            decoration: BoxDecoration(
-                              color: scheme.surfaceVariant,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                          Container(
-                            height: 8,
-                            width: 8,
-                            decoration: BoxDecoration(
-                              color: scheme.surfaceVariant,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
-              const Spacer(),
-              Text(name)
-            ],
-          )),
+            ),
+            const Spacer(),
+            Text(name),
+          ],
+        ),
+      ),
     );
   }
 }

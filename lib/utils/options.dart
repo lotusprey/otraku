1751b200be7f1804ffafeecbfe05595b5abe1bce
lotusgate <lotus.gate.dev@gmail.com>
2022-10-13 23:42:38 +0300

    Fixed the app crashing, because of an empty collection.

--- lib/utils/options.dart
+++ lib/utils/options.dart
@@ -7,7 +7,7 @@ import 'package:otraku/utils/theming.dart';
 import 'package:path_provider/path_provider.dart';
 
 /// Current app version.
-const versionCode = '1.2.1+3';
+const versionCode = '1.2.1+4';
 
 /// General options keys.
 enum _OptionKey {

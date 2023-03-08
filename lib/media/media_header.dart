b9848e9f0ea292acb935e541029c36fc9d226626
lotusgate <lotus.gate.dev@gmail.com>
2023-03-08 18:40:06 +0200

    Code is now not shared between user/media header as it didn't make things simpler.

--- lib/media/media_header.dart
+++ lib/media/media_header.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:otraku/discover/discover_models.dart';
+import 'package:otraku/media/media_models.dart';
 import 'package:otraku/media/media_providers.dart';
+import 'package:otraku/utils/consts.dart';
 import 'package:otraku/utils/convert.dart';
-import 'package:otraku/widgets/custom_sliver_header.dart';
+import 'package:otraku/widgets/fade_image.dart';
 import 'package:otraku/widgets/layouts/top_bar.dart';
+import 'package:otraku/widgets/overlays/dialogs.dart';
 import 'package:otraku/widgets/overlays/sheets.dart';
 import 'package:otraku/widgets/overlays/toast.dart';
 import 'package:otraku/widgets/text_rail.dart';
@@ -19,98 +23,300 @@ class MediaHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(
-      builder: (context, ref, _) => ref.watch(mediaProvider(id)).when(
-            loading: _placeholder,
-            error: (_, __) => _placeholder(),
-            data: (data) {
-              final info = data.info;
-              final textRailItems = <String, bool>{};
+      builder: (context, ref, _) {
+        final data = ref.watch(mediaProvider(id).select((s) => s.valueOrNull));
+        final textRailItems = <String, bool>{};
 
-              if (info.isAdult) textRailItems['Adult'] = true;
-              if (info.format != null) {
-                textRailItems[Convert.clarifyEnum(info.format)!] = false;
-              }
-              if (data.edit.status != null) {
-                textRailItems[Convert.adaptListStatus(
-                  data.edit.status!,
-                  info.type == DiscoverType.anime,
-                )] = false;
-              }
-              if (info.airingAt != null) {
-                textRailItems['Ep ${info.nextEpisode} in '
-                    '${Convert.timeUntilTimestamp(info.airingAt)}'] = true;
-              }
-              if (data.edit.status != null) {
-                final progress = data.edit.progress;
-                if (info.nextEpisode != null &&
-                    info.nextEpisode! - 1 > progress) {
-                  textRailItems['${info.nextEpisode! - 1 - progress}'
-                      ' ep behind'] = true;
-                }
-              }
+        if (data != null) {
+          final info = data.info;
 
-              return CustomSliverHeader(
-                title: info.preferredTitle,
-                image: info.cover,
-                extraLargeImage: info.extraLargeCover,
-                banner: info.banner,
-                squareImage: false,
-                implyLeading: true,
-                heroId: id,
-                maxWidth: null,
-                actions: [
-                  if (info.siteUrl != null)
+          if (info.isAdult) textRailItems['Adult'] = true;
+
+          if (info.format != null) {
+            textRailItems[Convert.clarifyEnum(info.format)!] = false;
+          }
+
+          if (data.edit.status != null) {
+            textRailItems[Convert.adaptListStatus(
+              data.edit.status!,
+              info.type == DiscoverType.anime,
+            )] = false;
+          }
+
+          if (info.airingAt != null) {
+            textRailItems['Ep ${info.nextEpisode} in '
+                '${Convert.timeUntilTimestamp(info.airingAt)}'] = true;
+          }
+
+          if (data.edit.status != null) {
+            final progress = data.edit.progress;
+            if (info.nextEpisode != null && info.nextEpisode! - 1 > progress) {
+              textRailItems['${info.nextEpisode! - 1 - progress}'
+                  ' ep behind'] = true;
+            }
+          }
+        }
+
+        return SliverPersistentHeader(
+          pinned: true,
+          delegate: _Delegate(
+            id: id,
+            info: data?.info,
+            coverUrl: coverUrl,
+            textRailItems: textRailItems,
+            imageWidth: MediaQuery.of(context).size.width < 430.0
+                ? MediaQuery.of(context).size.width * 0.30
+                : 100.0,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _Delegate implements SliverPersistentHeaderDelegate {
+  _Delegate({
+    required this.id,
+    required this.info,
+    required this.imageWidth,
+    required this.coverUrl,
+    required this.textRailItems,
+  });
+
+  final int id;
+  final MediaInfo? info;
+  final double imageWidth;
+  final String? coverUrl;
+  final Map<String, bool> textRailItems;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final height = maxExtent;
+    final extent = height - shrinkOffset;
+    final opacity = shrinkOffset < (_bannerHeight - minExtent)
+        ? shrinkOffset / (_bannerHeight - minExtent)
+        : 1.0;
+
+    final cover = info?.cover ?? coverUrl;
+    final theme = Theme.of(context);
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surfaceVariant,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 5,
+            spreadRadius: 5,
+            color: theme.colorScheme.background,
+          ),
+        ],
+      ),
+      child: FlexibleSpaceBar.createSettings(
+        minExtent: minExtent,
+        maxExtent: height,
+        currentExtent: extent > minExtent ? extent : minExtent,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              stretchModes: const [StretchMode.zoomBackground],
+              background: Column(
+                children: [
+                  Expanded(
+                    child: info?.banner != null
+                        ? GestureDetector(
+                            child: FadeImage(info!.banner!),
+                            onTap: () => showPopUp(
+                              context,
+                              ImageDialog(info!.banner!),
+                            ),
+                          )
+                        : const SizedBox(),
+                  ),
+                  SizedBox(height: height - _bannerHeight),
+                ],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: height - _bannerHeight,
+                alignment: Alignment.topCenter,
+                color: theme.colorScheme.background,
+                child: Container(
+                  height: 0,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 25,
+                        color: theme.colorScheme.background,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 10,
+              right: 10,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Hero(
+                    tag: id,
+                    child: ClipRRect(
+                      borderRadius: Consts.borderRadiusMin,
+                      child: Container(
+                        height: imageHeight,
+                        width: imageWidth,
+                        color: theme.colorScheme.surfaceVariant,
+                        child: cover != null
+                            ? GestureDetector(
+                                onTap: () => showPopUp(
+                                  context,
+                                  ImageDialog(
+                                    info?.extraLargeCover ?? cover,
+                                  ),
+                                ),
+                                child: FadeImage(cover),
+                              )
+                            : null,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        if (info?.preferredTitle != null)
+                          GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () =>
+                                Toast.copy(context, info!.preferredTitle!),
+                            child: Text(
+                              info!.preferredTitle!,
+                              maxLines: 8,
+                              overflow: TextOverflow.fade,
+                              style: theme.textTheme.titleLarge!.copyWith(
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 10,
+                                    color: theme.colorScheme.background,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        if (textRailItems.isNotEmpty)
+                          TextRail(
+                            textRailItems,
+                            style: theme.textTheme.labelMedium,
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: minExtent,
+              child: Opacity(
+                opacity: opacity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.background,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        spreadRadius: 10,
+                        color: theme.colorScheme.background,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: minExtent,
+              child: Row(
+                children: [
+                  TopBarShadowIcon(
+                    tooltip: 'Close',
+                    icon: Ionicons.chevron_back_outline,
+                    onTap: Navigator.of(context).pop,
+                  ),
+                  Expanded(
+                    child: info?.preferredTitle == null
+                        ? const SizedBox()
+                        : Opacity(
+                            opacity: opacity,
+                            child: Text(
+                              info!.preferredTitle!,
+                              style: theme.textTheme.titleMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                  ),
+                  if (info?.siteUrl != null)
                     TopBarShadowIcon(
                       tooltip: 'More',
                       icon: Ionicons.ellipsis_horizontal,
                       onTap: () => showSheet(
                         context,
-                        FixedGradientDragSheet.link(context, info.siteUrl!),
+                        FixedGradientDragSheet.link(context, info!.siteUrl!),
                       ),
                     ),
                 ],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () => Toast.copy(context, info.preferredTitle!),
-                      child: Text(
-                        info.preferredTitle!,
-                        maxLines: 8,
-                        overflow: TextOverflow.fade,
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          shadows: [
-                            Shadow(
-                              blurRadius: 10,
-                              color: Theme.of(context).colorScheme.background,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    TextRail(
-                      textRailItems,
-                      style: Theme.of(context).textTheme.labelMedium,
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget _placeholder() => CustomSliverHeader(
-        heroId: id,
-        image: coverUrl,
-        squareImage: false,
-        implyLeading: true,
-        actions: const [],
-        title: null,
-        banner: null,
-        maxWidth: null,
-        child: null,
-      );
+  static const _bannerHeight = 200.0;
+
+  double get imageHeight => imageWidth * Consts.coverHtoWRatio;
+
+  @override
+  double get maxExtent => _bannerHeight + imageHeight / 2;
+
+  @override
+  double get minExtent => Consts.tapTargetSize;
+
+  @override
+  OverScrollHeaderStretchConfiguration? get stretchConfiguration =>
+      OverScrollHeaderStretchConfiguration(stretchTriggerOffset: 100);
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
+
+  @override
+  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration =>
+      null;
+
+  @override
+  FloatingHeaderSnapConfiguration? get snapConfiguration => null;
+
+  @override
+  TickerProvider? get vsync => null;
 }
--- lib/user/user_header.dart
+++ lib/user/user_header.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:otraku/utils/consts.dart';
 import 'package:otraku/user/user_models.dart';
 import 'package:otraku/user/user_providers.dart';
 import 'package:otraku/utils/route_arg.dart';
-import 'package:otraku/widgets/custom_sliver_header.dart';
+import 'package:otraku/widgets/fade_image.dart';
 import 'package:otraku/widgets/layouts/top_bar.dart';
 import 'package:otraku/widgets/overlays/dialogs.dart';
 import 'package:otraku/widgets/overlays/sheets.dart';
@@ -14,14 +15,14 @@ import 'package:otraku/widgets/text_rail.dart';
 class UserHeader extends StatelessWidget {
   const UserHeader({
     required this.id,
-    required this.user,
     required this.isMe,
+    required this.user,
     required this.imageUrl,
   });
 
   final int id;
-  final User? user;
   final bool isMe;
+  final User? user;
   final String? imageUrl;
 
   @override
@@ -32,76 +33,297 @@ class UserHeader extends StatelessWidget {
       if (user!.donatorTier > 0) textRailItems[user!.donatorBadge] = true;
     }
 
-    return CustomSliverHeader(
-      title: user?.name,
-      image: user?.imageUrl ?? imageUrl,
-      banner: user?.bannerUrl,
-      squareImage: true,
-      implyLeading: !isMe,
-      heroId: id,
-      actions: [
-        if (!isMe && user != null) _FollowButton(user!),
-        if (user?.siteUrl != null)
-          TopBarShadowIcon(
-            tooltip: 'More',
-            icon: Ionicons.ellipsis_horizontal,
-            onTap: () => showSheet(
-              context,
-              FixedGradientDragSheet.link(context, user!.siteUrl!),
-            ),
-          ),
-        if (isMe)
-          TopBarShadowIcon(
-            tooltip: 'Settings',
-            icon: Ionicons.cog_outline,
-            onTap: () => Navigator.pushNamed(context, RouteArg.settings),
-          ),
-      ],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: user != null
-            ? [
-                GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () => Toast.copy(context, user!.name),
-                  child: Text(
-                    user!.name,
-                    overflow: TextOverflow.fade,
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      shadows: [
-                        Shadow(
-                          color: Theme.of(context).colorScheme.background,
-                          blurRadius: 10,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                if (textRailItems.isNotEmpty)
-                  GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: () {
-                      if (user!.modRoles.isNotEmpty) {
-                        showPopUp(
-                          context,
-                          TextDialog(
-                            title: 'Roles',
-                            text: user!.modRoles.join(', '),
-                          ),
-                        );
-                      }
-                    },
-                    child: TextRail(
-                      textRailItems,
-                      style: Theme.of(context).textTheme.labelMedium,
-                    ),
-                  ),
-              ]
-            : [],
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _Delegate(
+        id: id,
+        isMe: isMe,
+        user: user,
+        imageUrl: imageUrl,
+        textRailItems: textRailItems,
+        imageWidth: MediaQuery.of(context).size.width < 430.0
+            ? MediaQuery.of(context).size.width * 0.30
+            : 100.0,
       ),
     );
   }
+}
+
+class _Delegate implements SliverPersistentHeaderDelegate {
+  _Delegate({
+    required this.id,
+    required this.isMe,
+    required this.user,
+    required this.imageUrl,
+    required this.imageWidth,
+    required this.textRailItems,
+  });
+
+  final int id;
+  final bool isMe;
+  final User? user;
+  final String? imageUrl;
+  final double imageWidth;
+  final Map<String, bool> textRailItems;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final sidePadding =
+        MediaQuery.of(context).size.width > Consts.layoutBig + 20
+            ? (MediaQuery.of(context).size.width - Consts.layoutBig) / 2
+            : 10.0;
+
+    final height = maxExtent;
+    final extent = maxExtent - shrinkOffset;
+    final opacity = shrinkOffset < (_bannerHeight - minExtent)
+        ? shrinkOffset / (_bannerHeight - minExtent)
+        : 1.0;
+
+    final image = user?.imageUrl ?? imageUrl;
+    final theme = Theme.of(context);
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surfaceVariant,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 5,
+            spreadRadius: 5,
+            color: theme.colorScheme.background,
+          ),
+        ],
+      ),
+      child: FlexibleSpaceBar.createSettings(
+        minExtent: minExtent,
+        maxExtent: maxExtent,
+        currentExtent: extent > minExtent ? extent : minExtent,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              stretchModes: const [StretchMode.zoomBackground],
+              background: Column(
+                children: [
+                  Expanded(
+                    child: user?.bannerUrl != null
+                        ? GestureDetector(
+                            child: FadeImage(user!.bannerUrl!),
+                            onTap: () => showPopUp(
+                              context,
+                              ImageDialog(user!.bannerUrl!),
+                            ),
+                          )
+                        : const SizedBox(),
+                  ),
+                  SizedBox(height: height - _bannerHeight),
+                ],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: height - _bannerHeight,
+                alignment: Alignment.topCenter,
+                color: theme.colorScheme.background,
+                child: Container(
+                  height: 0,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 25,
+                        color: theme.colorScheme.background,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: sidePadding,
+              right: sidePadding,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Hero(
+                    tag: id,
+                    child: ClipRRect(
+                      borderRadius: Consts.borderRadiusMin,
+                      child: SizedBox(
+                        height: imageWidth,
+                        width: imageWidth,
+                        child: image != null
+                            ? GestureDetector(
+                                onTap: () => showPopUp(
+                                  context,
+                                  ImageDialog(image),
+                                ),
+                                child: FadeImage(
+                                  image,
+                                  fit: BoxFit.contain,
+                                  alignment: Alignment.bottomCenter,
+                                ),
+                              )
+                            : null,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (user != null)
+                          GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () => Toast.copy(context, user!.name),
+                            child: Text(
+                              user!.name,
+                              overflow: TextOverflow.fade,
+                              style: theme.textTheme.titleLarge!.copyWith(
+                                shadows: [
+                                  Shadow(
+                                    color: theme.colorScheme.background,
+                                    blurRadius: 10,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        if (textRailItems.isNotEmpty)
+                          GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () {
+                              if (user?.modRoles.isNotEmpty ?? false) {
+                                showPopUp(
+                                  context,
+                                  TextDialog(
+                                    title: 'Roles',
+                                    text: user!.modRoles.join(', '),
+                                  ),
+                                );
+                              }
+                            },
+                            child: TextRail(
+                              textRailItems,
+                              style: theme.textTheme.labelMedium,
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: minExtent,
+              child: Opacity(
+                opacity: opacity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.background,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        spreadRadius: 10,
+                        color: theme.colorScheme.background,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: minExtent,
+              child: Row(
+                children: [
+                  isMe
+                      ? const SizedBox(width: 10)
+                      : TopBarShadowIcon(
+                          tooltip: 'Close',
+                          icon: Ionicons.chevron_back_outline,
+                          onTap: Navigator.of(context).pop,
+                        ),
+                  Expanded(
+                    child: user?.name == null
+                        ? const SizedBox()
+                        : Opacity(
+                            opacity: opacity,
+                            child: Text(
+                              user!.name,
+                              style: theme.textTheme.titleMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                  ),
+                  if (!isMe && user != null) _FollowButton(user!),
+                  if (user?.siteUrl != null)
+                    TopBarShadowIcon(
+                      tooltip: 'More',
+                      icon: Ionicons.ellipsis_horizontal,
+                      onTap: () => showSheet(
+                        context,
+                        FixedGradientDragSheet.link(context, user!.siteUrl!),
+                      ),
+                    ),
+                  if (isMe)
+                    TopBarShadowIcon(
+                      tooltip: 'Settings',
+                      icon: Ionicons.cog_outline,
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        RouteArg.settings,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  static const _bannerHeight = 200.0;
+
+  @override
+  double get maxExtent => _bannerHeight + imageWidth / 2;
+
+  @override
+  double get minExtent => Consts.tapTargetSize;
+
+  @override
+  OverScrollHeaderStretchConfiguration? get stretchConfiguration =>
+      OverScrollHeaderStretchConfiguration(stretchTriggerOffset: 100);
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
+
+  @override
+  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration =>
+      null;
+
+  @override
+  FloatingHeaderSnapConfiguration? get snapConfiguration => null;
+
+  @override
+  TickerProvider? get vsync => null;
 }
 
 class _FollowButton extends StatefulWidget {

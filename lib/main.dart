b1615610ec498d214532929744c871df1e3a7ab0
lotusgate <lotus.gate.dev@gmail.com>
2023-03-18 18:53:48 +0200

    Cosmetic changes.

--- lib/main.dart
+++ lib/main.dart
@@ -36,95 +36,97 @@ class AppState extends State<App> {
   }
 
   @override
-  Widget build(BuildContext context) => Consumer(
-        builder: (context, ref, _) => DynamicColorBuilder(
-          builder: (lightDynamic, darkDynamic) {
-            ColorScheme lightScheme;
-            ColorScheme darkScheme;
-            var theme = Options().theme;
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
-            /// The system schemes must be cached, so
-            /// they can later be used in the settings.
-            final notifier = ref.watch(homeProvider.notifier);
-            final hasDynamic = lightDynamic != null && darkDynamic != null;
+    return Consumer(
+      builder: (context, ref, _) => DynamicColorBuilder(
+        builder: (lightDynamic, darkDynamic) {
+          ColorScheme lightScheme;
+          ColorScheme darkScheme;
+          var theme = Options().theme;
 
-            final darkBackground =
-                Options().pureBlackDarkTheme ? Colors.black : null;
+          /// The system schemes must be cached, so
+          /// they can later be used in the settings.
+          final notifier = ref.watch(homeProvider.notifier);
+          final hasDynamic = lightDynamic != null && darkDynamic != null;
 
-            if (hasDynamic) {
-              lightDynamic = lightDynamic.harmonized();
-              darkDynamic = darkDynamic.harmonized().copyWith(
-                    background: darkBackground,
-                  );
-              notifier.setSystemSchemes(lightDynamic, darkDynamic);
-            } else {
-              notifier.setSystemSchemes(null, null);
-            }
+          final darkBackground =
+              Options().pureBlackDarkTheme ? Colors.black : null;
 
-            if (theme == null && hasDynamic) {
-              lightScheme = lightDynamic!;
-              darkScheme = darkDynamic!;
-            } else {
-              theme ??= 0;
-              if (theme >= colorSeeds.length) {
-                theme = colorSeeds.length - 1;
-              }
-
-              final seed = colorSeeds.values.elementAt(theme);
-              lightScheme = seed.scheme(Brightness.light);
-              darkScheme = seed
-                  .scheme(Brightness.dark)
-                  .copyWith(background: darkBackground);
-            }
-
-            final mode = Options().themeMode;
-            final platform =
-                SchedulerBinding.instance.window.platformBrightness;
-            final isDark = mode == ThemeMode.system
-                ? platform == Brightness.dark
-                : mode == ThemeMode.dark;
-
-            final ColorScheme scheme;
-            final Brightness overlayBrightness;
-            if (isDark) {
-              scheme = darkScheme;
-              overlayBrightness = Brightness.light;
-            } else {
-              scheme = lightScheme;
-              overlayBrightness = Brightness.dark;
-            }
-
-            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarBrightness: scheme.brightness,
-              statusBarIconBrightness: overlayBrightness,
-              systemNavigationBarColor: scheme.background,
-              systemNavigationBarIconBrightness: overlayBrightness,
-            ));
-            final data = themeDataFrom(scheme);
-
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Otraku',
-              theme: data,
-              darkTheme: data,
-              navigatorKey: RouteArg.navKey,
-              onGenerateRoute: RouteArg.generateRoute,
-              builder: (context, child) {
-                /// Override the [textScaleFactor], because some devices apply
-                /// too high of a factor and it breaks the app visually.
-                /// [child] can't be null, because [onGenerateRoute] is provided.
-                final mediaQuery = MediaQuery.of(context);
-                final scale =
-                    mediaQuery.textScaleFactor.clamp(0.8, 1).toDouble();
-
-                return MediaQuery(
-                  data: mediaQuery.copyWith(textScaleFactor: scale),
-                  child: child!,
+          if (hasDynamic) {
+            lightDynamic = lightDynamic.harmonized();
+            darkDynamic = darkDynamic.harmonized().copyWith(
+                  background: darkBackground,
                 );
-              },
-            );
-          },
-        ),
-      );
+            notifier.setSystemSchemes(lightDynamic, darkDynamic);
+          } else {
+            notifier.setSystemSchemes(null, null);
+          }
+
+          if (theme == null && hasDynamic) {
+            lightScheme = lightDynamic!;
+            darkScheme = darkDynamic!;
+          } else {
+            theme ??= 0;
+            if (theme >= colorSeeds.length) {
+              theme = colorSeeds.length - 1;
+            }
+
+            final seed = colorSeeds.values.elementAt(theme);
+            lightScheme = seed.scheme(Brightness.light);
+            darkScheme = seed
+                .scheme(Brightness.dark)
+                .copyWith(background: darkBackground);
+          }
+
+          final mode = Options().themeMode;
+          final platform = SchedulerBinding.instance.window.platformBrightness;
+          final isDark = mode == ThemeMode.system
+              ? platform == Brightness.dark
+              : mode == ThemeMode.dark;
+
+          final ColorScheme scheme;
+          final Brightness overlayBrightness;
+          if (isDark) {
+            scheme = darkScheme;
+            overlayBrightness = Brightness.light;
+          } else {
+            scheme = lightScheme;
+            overlayBrightness = Brightness.dark;
+          }
+
+          SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarBrightness: scheme.brightness,
+            statusBarIconBrightness: overlayBrightness,
+            systemNavigationBarColor: Colors.transparent,
+            systemNavigationBarIconBrightness: overlayBrightness,
+          ));
+          final data = themeDataFrom(scheme);
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Otraku',
+            theme: data,
+            darkTheme: data,
+            navigatorKey: RouteArg.navKey,
+            onGenerateRoute: RouteArg.generateRoute,
+            builder: (context, child) {
+              /// Override the [textScaleFactor], because some devices apply
+              /// too high of a factor and it breaks the app visually.
+              /// [child] can't be null, because [onGenerateRoute] is provided.
+              final mediaQuery = MediaQuery.of(context);
+              final scale = mediaQuery.textScaleFactor.clamp(0.8, 1).toDouble();
+
+              return MediaQuery(
+                data: mediaQuery.copyWith(textScaleFactor: scale),
+                child: child!,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
